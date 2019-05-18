f9d438cf769892fb09c6b4e0caa3878760c2ddbb
JincomVR <donovan@jincom.co.za>
2019-05-18 11:49:49 +0200

    Basic scripts added
    dfjhsdjkfhsdjk

---
+++ The Game/Assets/Scripts/Core/GameConstants.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Jincom.Core
+{
+    public static class GameConstants
+    {
+        public enum Elements
+        {
+            Normal,
+            Fire,
+            Ice,
+            Acid,
+            Shock
+        };
+    }
+}
---
+++ The Game/Assets/Scripts/PickUps/WeaponData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Jincom.Core;
+
+namespace Jincom.PickUps
+{
+    [CreateAssetMenu(fileName = "WeaponData", menuName = "PickUp/Weapon")]
+    public class WeaponData : ScriptableObject
+    {
+        public string WeaponName = null;
+        public float Range = 0f;
+        public int Damage = 0;
+        public GameConstants.Elements Element = GameConstants.Elements.Normal;
+        
+
+    }
+}
