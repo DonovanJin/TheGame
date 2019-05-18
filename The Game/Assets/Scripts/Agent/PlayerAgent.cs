dbbe2b1d39481ff224859e06166b59504c1e00d1
HermannLaGrange <47741037+HermannLaGrange@users.noreply.github.com>
2019-05-18 13:36:26 +0200

    Agent Base
    Started Player Agent

---
+++ The Game/Assets/Scripts/Agent/PlayerAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jincom.Agent
+{
+    public class PlayerAgent : AgentBase
+    {
+        public void Start()
+        {
+            MoveAgent();
+        }
+
+        public override void MoveAgent()
+        {
+            base.MoveAgent();
+            Debug.Log("Move Player");
+        }
+    }
+}
