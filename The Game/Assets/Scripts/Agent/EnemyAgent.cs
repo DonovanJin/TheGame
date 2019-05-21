b3843d0cbf2a3c12a982dc3f484433275f77cafc
HermannLaGrange <47741037+HermannLaGrange@users.noreply.github.com>
2019-05-21 15:11:49 +0200

    Started work on Enemy Awareness of Player
    Features added:
    * Track distance to player
    * Line of sight to player
    
    When the player is unobstructed and within a minimum range, the enemy 'spots' the player.

--- The Game/Assets/Scripts/Agent/EnemyAgent.cs
+++ The Game/Assets/Scripts/Agent/EnemyAgent.cs
@@ -1,19 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Jincom.Agent
 {
     public class EnemyAgent : AgentBase
     {
+        [Header("Enemy Properties")]
+        public float CurrentDistanceToPlayer;
+        public float MinDistanceToPlayer = 1f;
+        public bool CloseEnough = false;
+        public bool SeePlayer = false;
+        public bool CorrectDirection = false;
+
+        private RaycastHit RayHit;
+
         public void Update()
         {
-            AgentUpdate();
+            AgentUpdate();            
         }
 
         public override void AgentUpdate()
         {
-            MoveAgent(Mathf.PingPong(Time.unscaledTime, 4f) - 2f);
+            //MoveAgent(Mathf.PingPong(Time.unscaledTime, 4f) - 2f);
+            DistanceToPlayer();
+            LineOfSight();
+        }
+
+        public void DistanceToPlayer()
+        {
+            CurrentDistanceToPlayer = Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position);
+
+            if(CurrentDistanceToPlayer <= MinDistanceToPlayer)
+            {
+                CloseEnough = true;
+            }
+            else
+            {
+                CloseEnough = false;
+            }
+        }
+
+        public void LineOfSight()
+        {
+            //if (Physics.Raycast(transform.position, (GameObject.FindGameObjectWithTag("Player").transform.position - transform.position), out hit, maxRange))
+            //{
+            //    if (hit.transform == player)
+            //    {
+            //        // In Range and i can see you!
+            //    }
+            //}
+            if (CloseEnough)
+            {
+                if (Physics.Raycast(transform.position, (GameObject.FindGameObjectWithTag("Player").transform.position - transform.position), out RayHit, MinDistanceToPlayer))
+                {
+                    if (RayHit.collider.tag == "Player")
+                    {
+                        Debug.Log("I see you.");
+                    }
+                }
+            }
         }
     }
 }
