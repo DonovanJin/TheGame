325f2088e97f1f774a5cfe3a044e042e4c39722a
HermannLaGrange <47741037+HermannLaGrange@users.noreply.github.com>
2019-05-22 15:18:44 +0200

    Added Camera Logic
    * Added a new Script [CameraLogic] to dictate Main Camera behavior.
    
    Camera Logic at the moment only follows the player, unless its target type is changed from 'Player' to 'other'. Currently it has no other target, so it will simply stop moving if the target type changes.

---
+++ The Game/Assets/Scripts/Agent/CameraLogic.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jincom.Camera
+{
+    public class CameraLogic : MonoBehaviour
+    {
+        public GameObject CameraTarget;
+        public float DistanceToTarget;
+
+        public enum TargetType
+        {
+            Player,
+            Other
+        };
+        public TargetType CameraTargetType;
+
+        void Update()
+        {
+            DecideOnTarget();
+            FollowTarget();
+        }
+
+        private void DecideOnTarget()
+        {
+            if (CameraTargetType == TargetType.Player)
+            {
+                if ((CameraTarget == null) || (CameraTarget.tag != "Player"))
+                {
+                    CameraTarget = GameObject.FindGameObjectWithTag("Player");
+                }
+            }
+            else
+            {
+                if (CameraTarget != null)
+                {
+                    if (CameraTarget.tag == "Player")
+                    {
+                        CameraTarget = null;
+                    }
+                }
+            }
+        }
+
+        private void FollowTarget()
+        {
+            if (CameraTarget != null)
+            {
+                transform.position = new Vector3(CameraTarget.transform.position.x, CameraTarget.transform.position.y, CameraTarget.transform.position.z - DistanceToTarget);
+            }
+        }
+    }
+}
