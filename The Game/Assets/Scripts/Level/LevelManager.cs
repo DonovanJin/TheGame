3cebf6b2cf5237163c729d959de5cb5d3bcda264
Jeff <jwho303@gmail.com>
2019-06-28 21:20:27 +0200

    Added manager classes

---
+++ The Game/Assets/Scripts/Level/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Jincom.PickUps;
+using Jincom.Agent;
+
+namespace Jincom.Level
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerManager _playerManager;
+
+        [SerializeField]
+        private EnemyManager _enemyManager;
+
+        [SerializeField]
+        private PickUpManager _pickUpManager;
+    }
+}
---
+++ The Game/Assets/Scripts/Level/LevelSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jincom.Level
+{
+    public class LevelSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private LevelManager _prefab;
+
+        [SerializeField]
+        private LevelManager _levelManager;
+    }
+}
